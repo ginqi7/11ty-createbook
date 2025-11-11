c0ee9683f55ba3b31dd78d6653e21ac66f5ec88e
Qiqi Jin <ginqi7@gmail.com>
2025-11-11 20:44:53 +0800

    Add the current date and time as the CSS version.

--- .eleventy.js
+++ .eleventy.js
@@ -64,6 +64,18 @@ export default function (eleventyConfig) {
     eleventyConfig.addFilter("bookTitle", bookTitle);
     eleventyConfig.addFilter("language", language);
     eleventyConfig.addFilter("limitN", limitN);
+    eleventyConfig.addFilter("version", (path) => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`;
+        return `${path}?v=${timestamp}`;
+    });
+
 
     eleventyConfig.addGlobalData("layout", "page");
     eleventyConfig.addCollection("pages", function (collectionApi) {
