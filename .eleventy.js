d568ac06055b1b92324e8397dbc0b5f4917eb466
Qiqi Jin <ginqi7@gmail.com>
2026-06-02 13:51:20 +0800

    feat: integrate html-douban-card filter for automatic Douban link conversion

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import replaceDoubanUrls from "html-douban-card";
 import pluginWordcount from "./plugins/wordcount.js";
 import pluginOrgMode from "./plugins/org-mode.js";
 
@@ -81,6 +82,9 @@ export default function (eleventyConfig) {
     const seconds = String(date.getSeconds()).padStart(2, "0");
     const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`;
     return `${path}?v=${timestamp}`;
+  });
+  eleventyConfig.addFilter("douban_card", async function (content) {
+    return await replaceDoubanUrls(content);
   });
 
   eleventyConfig.addGlobalData("layout", "page");
